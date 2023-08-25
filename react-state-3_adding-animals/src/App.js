91cf77f7c9e5e2641c777068a9353783514643ca
Iryna <119431403+nvhsk@users.noreply.github.com>
2023-08-25 11:42:25 +0200

    done

---
+++ react-state-3_adding-animals/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./components/Form/Form.js";
+import List from "./components/List/List.js";
+import { uid } from "uid";
+
+const initialAnimals = [
+  {
+    id: "mTBErig",
+    name: "Octopus",
+    emoji: "🐙",
+  },
+  {
+    id: "iMsbXQ1",
+    name: "Crocodile",
+    emoji: "🐊",
+  },
+  {
+    id: "Qjf_K7V",
+    name: "Beaver",
+    emoji: "🦫",
+  },
+];
+
+export default function App() {
+  const [animals, setAnimals] = useState(initialAnimals);
+
+  function handleAddAnimal(newAnimal) {
+    console.log(newAnimal);
+// adds new animal to the list ⬇️
+    setAnimals([...animals, { id: uid(), ...newAnimal }]);
+  }
+
+  return (
+    <main className="app">
+      <Form onAddAnimal={handleAddAnimal} />
+      <List animals={animals} />
+    </main>
+  );
+}
